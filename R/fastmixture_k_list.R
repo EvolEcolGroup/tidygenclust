76a5229356a0d816d05775cc8c5434db1f31b6c8
Cecilia Padilla <ceciliapad28@gmail.com>
2024-09-30 09:06:52 +0200

    vignette updated and linked to cumpling

--- R/fastmixture_k_list.R
+++ R/fastmixture_k_list.R
@@ -17,6 +17,8 @@
 #' @param als_iter Number of ALS (alternating least squares) iterations. Default is 1000.
 #' @param als_tole Tolerance value for ALS convergence. Default is 1e-4.
 #' @param no_freqs Logical parameter to indicate whether to use allele frequencies. Default is TRUE.
+#' @param save_q Logical parameter indicating whether to save each Q matrix as a file. Default is `FALSE`.
+#' @param output_path The path where Q files should be saved if `save_q` is `TRUE`. Default is the current working directory.
 #' @param random_init Logical parameter to indicate whether to use random initialization. Default is TRUE.
 #' @return A list of `q_matrix` objects, one for each K value provided.
 #' @examples
@@ -25,6 +27,7 @@
 #' result <- fastmixture_k_list(agt_object, k_values)
 #' }
 #' @export
+#' 
 
 fastmixture_k_list <- function(gt_object, k_values,
                                threads = 1, seed = 42, 
@@ -33,15 +36,27 @@ fastmixture_k_list <- function(gt_object, k_values,
                                check = 5, power = 11, 
                                chunk = 8192, als_iter = 1000, 
                                als_tole = 1e-4, no_freqs = TRUE, 
-                               random_init = TRUE) {
+                               random_init = TRUE, save_q = FALSE, 
+                               output_path = getwd()) {
+  
+  # Ensure the output directory exists if save_q is TRUE
+  if (save_q && !dir.exists(output_path)) {
+    dir.create(output_path, recursive = TRUE)
+  }
   
   # Run fastmixture for each K in k_values and store the results in a list
   qmat_list <- lapply(k_values, function(k) {
-    fastmixture(gt_object, k = k, threads = threads, seed = seed, 
+    q_matrix <- fastmixture(gt_object, k = k, threads = threads, seed = seed, 
                 outprefix = paste0("fastmixture_k", k), iter = iter, tole = tole, 
                 batches = batches, supervised = supervised, check = check, 
                 power = power, chunk = chunk, als_iter = als_iter, als_tole = als_tole, 
                 no_freqs = no_freqs, random_init = random_init)
+    # If save_q is TRUE, save the Q matrix as a file in the specified output_path
+    if (save_q) {
+      q_filename <- file.path(output_path, paste0("fastmixture_k", k, ".Q"))
+      write.table(q_matrix, file = q_filename, row.names = FALSE, col.names = F, quote = FALSE)
+    }
+    return(q_matrix)
   })
   
   # Return the list of q_matrix objects
