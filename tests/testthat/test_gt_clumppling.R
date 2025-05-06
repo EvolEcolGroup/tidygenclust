ae8b09060e69a118677f25455e43d1767b380072
Andrea Manica <am315@cam.ac.uk>
2025-05-06 06:36:04 +0100

    A few test notes

--- tests/testthat/test_gt_clumppling.R
+++ tests/testthat/test_gt_clumppling.R
@@ -1,5 +1,10 @@
+# skip if the conda environment does not exist
+skip_if(!reticulate::condaenv_exists("cclumppling"))
+
 input_path <- system.file("extdata/capeverde.zip", package = "tidygenclust")
 clump_res <- input_path %>% gt_clumppling()
+# TODO we need to check that what we return here is in the form we expect
+
 get_modes <- tidy(clump_res,matrix="modes")
 get_major_modes <- tidy(clump_res,matrix="major_modes")
 get_q_modes <- tidy(clump_res,matrix="q_modes")
