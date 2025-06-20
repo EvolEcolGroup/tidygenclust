8acde2727239a4ee86c76f43a41c3a9f31ae9b56
Andrea Manica <am315@cam.ac.uk>
2025-06-20 21:51:28 +0100

    More tests

---
+++ tests/testthat/test_gt_admixture.R
@@ -0,0 +1,40 @@
+# set the input file
+vcf_path <-
+  system.file(
+    "/extdata/anolis/punctatus_t70_s10_n46_filtered.recode.vcf.gz",
+    package = "tidypopgen"
+  )
+anole_gt <- tidypopgen::gen_tibble(
+  vcf_path,
+  quiet = TRUE,
+  backingfile = tempfile("anolis_")
+)
+pops_path <- system.file(
+  "/extdata/anolis/plot_order_punctatus_n46.csv",
+  package = "tidypopgen"
+)
+pops <- readr::read_csv(pops_path, show_col_types = FALSE)
+anole_gt <- anole_gt %>% dplyr::mutate(id = gsub("punc_", "", .data$id, ))
+anole_gt <- anole_gt %>%
+  dplyr::mutate(population = pops$pop[match(pops$ID, .data$id)])
+
+test_that("run admixture as single run", {
+  # we create a plink file to test the function
+  anole_plink <- tidypopgen::gt_as_plink(
+    anole_gt,
+    file = tempfile(),
+    chromosomes_as_int = TRUE
+  )
+  # run admixture
+  anole_adm <- tidypopgen::gt_admixture(
+    anole_plink,
+    k = 3,
+    crossval = FALSE,
+    n_cores = 1,
+    seed = 123
+  )
+  # check the output
+  expect_true(nrow(anole_adm$Q[[1]]) == nrow(anole_gt))
+  expect_true(ncol(anole_adm$Q[[1]]) == 3)
+  expect_true(is.null(anole_adm$cv))
+})
