be38a525a6a2ec59f9b9245cce90f17226881407
Andrea Manica <am315@cam.ac.uk>
2025-09-15 16:59:58 +0100

    Give NA if a piece of software is not installed

--- R/tgc_tools_version.R
+++ R/tgc_tools_version.R
@@ -6,29 +6,45 @@
 #' @export
 
 tgc_tools_version <- function() {
-  fast_ver <- reticulate::conda_run2(
-    cmd = "fastmixture",
-    args = c("--version"),
-    envname = "ctidygenclust",
-    echo = FALSE,
-    intern = TRUE
-  )
-  clump_ver <- reticulate::conda_run2(
-    cmd = "pip",
-    args = c("show clumppling | grep Version"),
-    envname = "cclumppling",
-    echo = FALSE,
-    intern = TRUE
-  )
-  tgc_vers <- list(fastmixture = fast_ver, clumppling = clump_ver)
-  if (.Platform$OS.type %in% c("unix", "darwin")) {
-    adm_ver <- reticulate::conda_run2(
-      cmd = "admixture",
+  fast_ver <- tryCatch(
+    reticulate::conda_run2(
+      cmd = "fastmixture",
       args = c("--version"),
       envname = "ctidygenclust",
       echo = FALSE,
       intern = TRUE
+    ),
+    error = function(e) NA,
+    warning = function(w) NA
+  )
+
+  clump_ver <- tryCatch(
+    reticulate::conda_run2(
+      cmd = "pip",
+      args = c("show clumppling | grep Version"),
+      envname = "cclumppling",
+      echo = FALSE,
+      intern = TRUE
+    ),
+    error = function(e) NA,
+    warning = function(w) NA
+  )
+
+  tgc_vers <- list(fastmixture = fast_ver, clumppling = clump_ver)
+
+  if (.Platform$OS.type %in% c("unix", "darwin")) {
+    adm_ver <- tryCatch(
+      reticulate::conda_run2(
+        cmd = "admixture",
+        args = c("--version"),
+        envname = "ctidygenclust",
+        echo = FALSE,
+        intern = TRUE
+      ),
+      error = function(e) NA,
+      warning = function(w) NA
     )
+
     tgc_vers[["admxiture"]] <- adm_ver[length(adm_ver)]
   }
   return(tgc_vers)
