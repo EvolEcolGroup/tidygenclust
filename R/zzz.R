d1749072b010a4d4a93a62185f38a424fd46131f
Andrea Manica <am315@cam.ac.uk>
2026-05-30 16:03:50 +0100

    force conda env use

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,20 @@
 # handles for the python modules
 .py_rfastmixture <- NULL
 .onLoad <- function(...) {
-  .py_rfastmixture <<- reticulate::import_from_path(
-    module = "py_rfastmixture",
-    path = system.file("python", package = "tidygenclust"),
-    delay_load = FALSE
-  )
+  if (reticulate::condaenv_exists("ctidygenclust")) {
+    reticulate::use_condaenv("ctidygenclust", required = TRUE)
+    .py_rfastmixture <<- reticulate::import_from_path(
+      module = "py_rfastmixture",
+      path = system.file("python", package = "tidygenclust"),
+      delay_load = FALSE
+    )
+  } else {
+    packageStartupMessage(
+      "The conda environment 'ctidygenclust' does not exist. Install it ",
+      "with 'tgc_tools_install()'"
+    )
+  }
+
 }
 .onAttach <- function(...) {
   if (reticulate::condaenv_exists("ctidygenclust")) {
